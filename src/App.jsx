88a24643868afd157ac76967d8eda6af366db453
Vera <maman1992@qq.com>
2022-08-14 18:59:09 +0800

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Home from './Home';
 import data from './data';
